d3ac9c6ac983f90e703e8c0cabc7b48632d0779c
MUHAMMAD_USMAN <105670679+msumanned@users.noreply.github.com>
2023-02-20 10:17:01 +0500

    main

---
+++ Chapter no 15/app.js
@@ -0,0 +1,21 @@
+// var bakerys =["cake","bread","paapey","eggs"]
+
+// var askWhichProduct =prompt("Which product you want to buy")
+
+// if(bakerys.indexOf(askWhichProduct)>=0){
+//     alert(`yes sir ${askWhichProduct} is avaible`)
+// }
+// else{
+//     alert(`sorry sir ${askWhichProduct} is not avaible`)
+// }
+
+// var carShouroom =["BMW","Revo","Mercedies","Vigo"]
+
+// var askWhichCar =prompt("Which car you want to buy")
+
+// if(carShouroom.indexOf(askWhichCar)>=0){
+//     alert(`yes sir ${askWhichCar} is avaible`)
+// }
+// else{
+//     alert(`sorry sir ${askWhichCar} is not avaible`)
+// }
